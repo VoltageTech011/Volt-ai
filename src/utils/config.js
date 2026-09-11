170c639755a28940ec98c7bf44f9092dba3d5ace
VoltageTech011 <voltagelordtech@gmail.com>
2026-09-11 09:50:33 +0100

    Delete src directory

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../../.env') });
-
-module.exports = {
-  port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV || 'development',
-  appName: process.env.APP_NAME || 'Voltage',
-  version: process.env.VERSION || '1.0.0'
-};
